a948268800ba8ca232bff0dc07bfaf17a793a52c
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-10-09 20:25:07 +0200

    hasta el 10b

---
+++ BtnComm.js
@@ -0,0 +1,4 @@
+  // Función para redirigir a index.html
+  function redirigirAIndex() {
+    window.location.href = "./index.html"; // Cambia "index.html" por la URL de la página a la que desees redirigir
+}
